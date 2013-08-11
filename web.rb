7d636fd6ad34c20f394ce7d81c11715827716118
Christian Williams <xian@squareup.com>
2013-08-11 02:31:31 -0700

    Redirect /camp to camp form.

--- web.rb
+++ web.rb
@@ -7,6 +7,10 @@ get '/' do
     File.read(File.join('public', 'index.html'))
 end
 
+get '/camp' do
+    redirect "http://baaahs.wufoo.com/forms/baaahs-on-playa-questionnaire/"
+end
+
 not_found do
   'This sheep intentionally left blank.'
 end
