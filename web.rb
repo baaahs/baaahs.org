ec906d4e8562634939f4b557b2a84c9f4fcc9e36
Christian Williams <xian@squareup.com>
2013-08-12 16:22:53 -0700

    Initial Flock Ewe camp doc.

--- web.rb
+++ web.rb
@@ -7,9 +7,9 @@ get '/' do
     File.read(File.join('public', 'index.html'))
 end
 
-get '/camp' do
-    redirect "http://baaahs.wufoo.com/forms/baaahs-on-playa-questionnaire/"
-end
+#get '/camp' do
+#    redirect "http://baaahs.wufoo.com/forms/baaahs-on-playa-questionnaire/"
+#end
 
 not_found do
   'This sheep intentionally left blank.'
