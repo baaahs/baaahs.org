cb439ea9c5a69eedd2b8c57a2742609ef7e60300
Christian Williams <antixian666@gmail.com>
2013-08-14 01:46:41 -0700

    More word cleanup. Add YES rule.

--- web.rb
+++ web.rb
@@ -7,6 +7,14 @@ get '/' do
     File.read(File.join('public', 'index.html'))
 end
 
+get '/camp' do
+  File.read(File.join('public', 'camp.html'))
+end
+
+get '/camp.html' do
+    redirect "/camp"
+end
+
 #get '/camp' do
 #    redirect "http://baaahs.wufoo.com/forms/baaahs-on-playa-questionnaire/"
 #end
