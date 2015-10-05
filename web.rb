271e13d99a30c95412eb2ae7a10abb0a08fa4bcc
Christian Williams <antixian666@gmail.com>
2015-10-05 13:49:18 -0700

    Simplify routing.

--- web.rb
+++ web.rb
@@ -4,31 +4,20 @@ require "sinatra/reloader" if development?
 
 set :bind, '0.0.0.0'
 
-get '/' do
-  # redirect "/volunteer2015.html"
-  File.read(File.join('public', 'index.html'))
-end
-
-get '/camp' do
-  File.read(File.join('public', 'camp.html'))
-end
-
-get '/camp.html' do
-  redirect "/camp"
-end
-
 get '/shifts' do
   redirect "http://www.volunteerspot.com/login/entry/375755452038"
 end
 
-#get '/camp' do
-#    redirect "http://baaahs.wufoo.com/forms/baaahs-on-playa-questionnaire/"
-#end
+get '/*' do |file|
+  puts "#{file} without html"
+  File.read(File.join('public', "#{file}.html"))
+end
 
-get '/faq/name' do
-  File.read(File.join('public', 'faq', 'name.html'))
+get '/' do
+  File.read(File.join('public', 'index.html'))
 end
 
 not_found do
   File.read(File.join('public', '404.html'))
 end
+
