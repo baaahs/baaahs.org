69b89459ad6994929b1deffed918478f1456980b
Christian Williams <antixian666@gmail.com>
2015-11-03 14:50:04 -0800

    Move /setup/setup to /setup.

--- web.rb
+++ web.rb
@@ -6,13 +6,27 @@ set :bind, '0.0.0.0'
 
 get('/shifts') { redirect "http://www.volunteerspot.com/login/entry/375755452038" }
 get('/drive') { redirect "https://drive.google.com/drive/folders/0B_TasILTM6TWa18zdHdmNHpUYzg" }
+get('/setup/setup') { redirect "/setup" }
 
 get '/' do
   File.read(File.join('public', 'index.html'))
 end
 
 get '/*' do |file|
-  File.read(File.join('public', "#{file}.html"))
+  file = file.gsub(/\.\./, '')
+
+  begin
+    File.read(File.join('public', "#{file}.html"))
+  rescue
+    dir_file = File.join('public', file)
+    index_file = File.join(dir_file, 'index.html')
+
+    if File.directory?(dir_file) && File.exists?(index_file)
+      File.read(index_file)
+    else
+      raise
+    end
+  end
 end
 
 not_found do
