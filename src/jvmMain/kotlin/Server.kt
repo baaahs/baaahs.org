ff2e724f66cebb08da5ac930a84c06937d32096e
Christian Williams <antixian666@gmail.com>
2023-02-21 10:38:46 -0800

    Add redirects back.

--- src/jvmMain/kotlin/Server.kt
+++ src/jvmMain/kotlin/Server.kt
@@ -189,6 +189,31 @@ fun Application.baaahsApplicationModule(port: Int, httpClient: HttpClient) {
             )
         }
 
+        // redirects to keep!
+        get("/drive") { call.respondRedirect("https://drive.google.com/drive/folders/0B_TasILTM6TWa18zdHdmNHpUYzg") }
+        get("/pspride") { call.respondRedirect("/psp/") }
+        get("/join") { call.respondRedirect("http://goo.gl/forms/XUvltyxql2") }
+
+        get("/assets") { call.respondRedirect("/assman/assets") }
+
+        get("/cal") { call.respondRedirect("https://calendar.google.com/calendar?cid=ODlydDZ0MWs1am1oMm9odnZicXBvbTZyMW9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ") }
+        get("/cal-private") { call.respondRedirect("https://calendar.google.com/calendar/embed?src=eo8lcds32ki40o14dr6m5t0o5s%40group.calendar.google.com&ctz=America%2FLos_Angeles") }
+        get("/slack") { call.respondRedirect("https://baaahs.slack.com") }
+        get("/slack-invite") { call.respondRedirect("https://join.slack.com/t/baaahs/shared_invite/enQtODg3MTE3NTk1ODA4LTRhMDEyZTY1MmI4YjIzN2JlYmUxMWQyNGMyYjA4MDhkMmMwMTU3YWFjOTVjNGZhZGY3YTc4MTNlZDE1NzFmMmY") }
+
+        // 2020
+        get("/2020") { call.respondRedirect("https://drive.google.com/drive/folders/1-5VGf1gZXyzGN1lYNVF1KwGBrJ6n9t4L") }
+        get("/2020-form") { call.respondRedirect("https://docs.google.com/forms/d/e/1FAIpQLSfuh4BvWp1q4eQ_W_sVCDsKmlOPPB1N5RekJHLTjCsR5qdeFQ/viewform") }
+        get("/2020-doc") { call.respondRedirect("https://docs.google.com/document/d/10Do2qdITwrQxGOeMGd7pe1jrIxQejF-W5jHK2wbagfE") }
+        get("/2020-sheet") { call.respondRedirect("https://docs.google.com/spreadsheets/d/11FeKLaktPhMq_Oh_mG8TBK3U9_eY2oHBGxE0T3GzLhU") }
+
+        // 2022
+        get("/crew") { call.respondRedirect("https://docs.google.com/document/d/11mQX1lZpP0rMNNV1Uni_J0hutXaMEhsjvKtermUuY6Q") }
+
+        // old URLs to support for a while!
+        get("/shifts") { call.respondRedirect("http://www.volunteerspot.com/login/entry/375755452038") } // todo kill after 20151201
+
+
         route(Asset.path) {
             get {
                 call.respond(collection.find().toList())
