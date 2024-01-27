bb286ae08b0022aa1f44ceef57e0573af0dde4b1
Tom Seago <tom@tomseago.com>
2024-01-26 21:42:58 -0800

    Maybe that was suppposed to be outside the block

--- devops/dev/main.tf
+++ devops/dev/main.tf
@@ -7,26 +7,27 @@ terraform {
             version = ">=5.13.0"
         }
     }
-    
-    resource "google_storage_bucket" "static-site" {
-        name          = "static.baaahs.org"
-        location      = "US"
-        force_destroy = true
+}
 
-        uniform_bucket_level_access = true
+resource "google_storage_bucket" "static-site" {
+    name          = "static.baaahs.org"
+    location      = "US"
+    force_destroy = true
 
-        website {
-            main_page_suffix = "index.html"
-            not_found_page   = "404.html"
-        }
-        cors {
-            origin          = ["https://static.baaahs.org"]
-            method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
-            response_header = ["*"]
-            max_age_seconds = 3600
-        }
+    uniform_bucket_level_access = true
+
+    website {
+        main_page_suffix = "index.html"
+        not_found_page   = "404.html"
+    }
+    cors {
+        origin          = ["https://static.baaahs.org"]
+        method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
+        response_header = ["*"]
+        max_age_seconds = 3600
     }
 }
+
 
 provider "google" {
     # Configuration options
