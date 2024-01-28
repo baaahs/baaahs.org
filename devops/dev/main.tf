8d2ee7d5c16517bc63be9f1acb22c88d85c99e52
Tom Seago <tom@tomseago.com>
2024-01-28 11:28:30 -0800

    Nuking dev infrastructure

--- devops/dev/main.tf
+++ devops/dev/main.tf
@@ -9,26 +9,6 @@ terraform {
     }
 }
 
-resource "google_storage_bucket" "static-site" {
-    name          = "static.baaahs.org"
-    location      = "US"
-    force_destroy = true
-
-    uniform_bucket_level_access = true
-
-    website {
-        main_page_suffix = "index.html"
-        not_found_page   = "404.html"
-    }
-    cors {
-        origin          = ["https://static.baaahs.org"]
-        method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
-        response_header = ["*"]
-        max_age_seconds = 3600
-    }
-}
-
-
 provider "google" {
     # Configuration options
     project = "baaahsorg-dev"
