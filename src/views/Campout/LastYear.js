ba51da10f5bab0e9aaec29f6ed5486d13485a064
Matt Rucker <vgmatt.rucker@gmail.com>
2024-01-28 23:20:35 -0800

    Add copy and cards to campout homepage

--- src/views/Campout/LastYear.js
+++ src/views/Campout/LastYear.js
@@ -1,43 +1,51 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import { useTheme } from '@mui/material/styles';
-import Avatar from '@mui/material/Avatar';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Grid';
 
 import CampoutMain from 'layouts/CampoutMain.js';
+import BAAAHSLogo from 'layouts/Main/BAAAHSLogo';
+
 const contentItems = [
   {
     name: 'Item 1',
     height: 300,
     width: 500,
+    src: 'https://lh3.googleusercontent.com/pw/ABLVV854juUtNiVdCR91ZX_YU2U51nqhSHke6YoLMpDDCCDXgP5y77HpgckJC2sWHpmbklj6IE6c7knkJ9oqSxlqa4y-wrYbBA_mRuxMUOesM_VZgwO_He3YCJoFcDBc4ppLCtCnktWUUKLLJt83xL7uBW8kxALYTKVsedcKRUG3WvL1bJiS179Huzt4epjjs8YgP6fbae-iT9zaszTP5sn_iuFIdEgM8e49cDjLBCwm6WmYHJKWdWk7CZzTWa3_hNdjihifdR1lpbYVQNop_pjzJcRHcJmak0-fU8gy8olBoyKX4QLJfsZNELmKrAx3E_WBfVL4If1gSc2i6RiSkCyUx2o-SxhZ4IQCqfcewzJzzO5Yp83W5Aq2LB_d255bFp3YDt7O4o0fbBocFciqFf3EMMhXAlJTttxxU0dZrDA8ilPmZic0JJjzkE8QG-3JneQ01lKLFwd49s9pFSiEJc29W0wq040jXx3hGCOmDbRmA2j9dpnw1ZupfgRBaE1PlXWd_BWYMPV7-dPIDDg1r8xK0mbn3dA3KO8HiUpsNsNGYpGMzE1Xk2iQVYfbvI7ZfVMHb-Zo-FcxiZweBLR-aUzgTrjlVoAJvoUIxQ-y8YjrK3D6quqEPLEkWGhEiCelH05Y_gfs7B11YQIA_R6Xs59ujWCKEGm2R28sg3NvE2H5oBYp4sMZvr06nVgzeq8M2q72ouMcAmf6SpGcSWxCJejIxsjEbN3xrCxOctTfXLnerZsJ9HLTahZZkGCDOfIB0pLFCMtwI6tgO82kkp4yWX7z11kXb4uKegftBZ2WLAUgf8VNe2QSxIdU2fi2ytxXeGQMapJp8vs6RcCdr7IvtwpmA8KrZ8iKR1jjKNjaQTxcscic1Q7nHH1JZ__cEFpH7OR82Rbe5Z_a__f1yTv0fWHAI9fK6Xm6QEI4FwuIL_xWtI-UivdAbQJmBA4JfcwxXHD4F2wlWEcMyGpUVz4xNiu1=w477-h357-no?authuser=0',
   },
   {
     name: 'Item 2',
     height: 300,
     width: 500,
+    src: 'https://lh3.googleusercontent.com/pw/ABLVV854juUtNiVdCR91ZX_YU2U51nqhSHke6YoLMpDDCCDXgP5y77HpgckJC2sWHpmbklj6IE6c7knkJ9oqSxlqa4y-wrYbBA_mRuxMUOesM_VZgwO_He3YCJoFcDBc4ppLCtCnktWUUKLLJt83xL7uBW8kxALYTKVsedcKRUG3WvL1bJiS179Huzt4epjjs8YgP6fbae-iT9zaszTP5sn_iuFIdEgM8e49cDjLBCwm6WmYHJKWdWk7CZzTWa3_hNdjihifdR1lpbYVQNop_pjzJcRHcJmak0-fU8gy8olBoyKX4QLJfsZNELmKrAx3E_WBfVL4If1gSc2i6RiSkCyUx2o-SxhZ4IQCqfcewzJzzO5Yp83W5Aq2LB_d255bFp3YDt7O4o0fbBocFciqFf3EMMhXAlJTttxxU0dZrDA8ilPmZic0JJjzkE8QG-3JneQ01lKLFwd49s9pFSiEJc29W0wq040jXx3hGCOmDbRmA2j9dpnw1ZupfgRBaE1PlXWd_BWYMPV7-dPIDDg1r8xK0mbn3dA3KO8HiUpsNsNGYpGMzE1Xk2iQVYfbvI7ZfVMHb-Zo-FcxiZweBLR-aUzgTrjlVoAJvoUIxQ-y8YjrK3D6quqEPLEkWGhEiCelH05Y_gfs7B11YQIA_R6Xs59ujWCKEGm2R28sg3NvE2H5oBYp4sMZvr06nVgzeq8M2q72ouMcAmf6SpGcSWxCJejIxsjEbN3xrCxOctTfXLnerZsJ9HLTahZZkGCDOfIB0pLFCMtwI6tgO82kkp4yWX7z11kXb4uKegftBZ2WLAUgf8VNe2QSxIdU2fi2ytxXeGQMapJp8vs6RcCdr7IvtwpmA8KrZ8iKR1jjKNjaQTxcscic1Q7nHH1JZ__cEFpH7OR82Rbe5Z_a__f1yTv0fWHAI9fK6Xm6QEI4FwuIL_xWtI-UivdAbQJmBA4JfcwxXHD4F2wlWEcMyGpUVz4xNiu1=w477-h357-no?authuser=0',
   },
   {
     name: 'Item 3',
     height: 300,
     width: 500,
+    src: 'https://lh3.googleusercontent.com/pw/ABLVV854juUtNiVdCR91ZX_YU2U51nqhSHke6YoLMpDDCCDXgP5y77HpgckJC2sWHpmbklj6IE6c7knkJ9oqSxlqa4y-wrYbBA_mRuxMUOesM_VZgwO_He3YCJoFcDBc4ppLCtCnktWUUKLLJt83xL7uBW8kxALYTKVsedcKRUG3WvL1bJiS179Huzt4epjjs8YgP6fbae-iT9zaszTP5sn_iuFIdEgM8e49cDjLBCwm6WmYHJKWdWk7CZzTWa3_hNdjihifdR1lpbYVQNop_pjzJcRHcJmak0-fU8gy8olBoyKX4QLJfsZNELmKrAx3E_WBfVL4If1gSc2i6RiSkCyUx2o-SxhZ4IQCqfcewzJzzO5Yp83W5Aq2LB_d255bFp3YDt7O4o0fbBocFciqFf3EMMhXAlJTttxxU0dZrDA8ilPmZic0JJjzkE8QG-3JneQ01lKLFwd49s9pFSiEJc29W0wq040jXx3hGCOmDbRmA2j9dpnw1ZupfgRBaE1PlXWd_BWYMPV7-dPIDDg1r8xK0mbn3dA3KO8HiUpsNsNGYpGMzE1Xk2iQVYfbvI7ZfVMHb-Zo-FcxiZweBLR-aUzgTrjlVoAJvoUIxQ-y8YjrK3D6quqEPLEkWGhEiCelH05Y_gfs7B11YQIA_R6Xs59ujWCKEGm2R28sg3NvE2H5oBYp4sMZvr06nVgzeq8M2q72ouMcAmf6SpGcSWxCJejIxsjEbN3xrCxOctTfXLnerZsJ9HLTahZZkGCDOfIB0pLFCMtwI6tgO82kkp4yWX7z11kXb4uKegftBZ2WLAUgf8VNe2QSxIdU2fi2ytxXeGQMapJp8vs6RcCdr7IvtwpmA8KrZ8iKR1jjKNjaQTxcscic1Q7nHH1JZ__cEFpH7OR82Rbe5Z_a__f1yTv0fWHAI9fK6Xm6QEI4FwuIL_xWtI-UivdAbQJmBA4JfcwxXHD4F2wlWEcMyGpUVz4xNiu1=w477-h357-no?authuser=0',
   },
   {
     name: 'Item 4',
     height: 300,
     width: 700,
+    src: 'https://lh3.googleusercontent.com/pw/ABLVV854juUtNiVdCR91ZX_YU2U51nqhSHke6YoLMpDDCCDXgP5y77HpgckJC2sWHpmbklj6IE6c7knkJ9oqSxlqa4y-wrYbBA_mRuxMUOesM_VZgwO_He3YCJoFcDBc4ppLCtCnktWUUKLLJt83xL7uBW8kxALYTKVsedcKRUG3WvL1bJiS179Huzt4epjjs8YgP6fbae-iT9zaszTP5sn_iuFIdEgM8e49cDjLBCwm6WmYHJKWdWk7CZzTWa3_hNdjihifdR1lpbYVQNop_pjzJcRHcJmak0-fU8gy8olBoyKX4QLJfsZNELmKrAx3E_WBfVL4If1gSc2i6RiSkCyUx2o-SxhZ4IQCqfcewzJzzO5Yp83W5Aq2LB_d255bFp3YDt7O4o0fbBocFciqFf3EMMhXAlJTttxxU0dZrDA8ilPmZic0JJjzkE8QG-3JneQ01lKLFwd49s9pFSiEJc29W0wq040jXx3hGCOmDbRmA2j9dpnw1ZupfgRBaE1PlXWd_BWYMPV7-dPIDDg1r8xK0mbn3dA3KO8HiUpsNsNGYpGMzE1Xk2iQVYfbvI7ZfVMHb-Zo-FcxiZweBLR-aUzgTrjlVoAJvoUIxQ-y8YjrK3D6quqEPLEkWGhEiCelH05Y_gfs7B11YQIA_R6Xs59ujWCKEGm2R28sg3NvE2H5oBYp4sMZvr06nVgzeq8M2q72ouMcAmf6SpGcSWxCJejIxsjEbN3xrCxOctTfXLnerZsJ9HLTahZZkGCDOfIB0pLFCMtwI6tgO82kkp4yWX7z11kXb4uKegftBZ2WLAUgf8VNe2QSxIdU2fi2ytxXeGQMapJp8vs6RcCdr7IvtwpmA8KrZ8iKR1jjKNjaQTxcscic1Q7nHH1JZ__cEFpH7OR82Rbe5Z_a__f1yTv0fWHAI9fK6Xm6QEI4FwuIL_xWtI-UivdAbQJmBA4JfcwxXHD4F2wlWEcMyGpUVz4xNiu1=w477-h357-no?authuser=0',
   },
   {
     name: 'Item 5',
     height: 500,
     width: 200,
+    src: 'https://lh3.googleusercontent.com/pw/ABLVV854juUtNiVdCR91ZX_YU2U51nqhSHke6YoLMpDDCCDXgP5y77HpgckJC2sWHpmbklj6IE6c7knkJ9oqSxlqa4y-wrYbBA_mRuxMUOesM_VZgwO_He3YCJoFcDBc4ppLCtCnktWUUKLLJt83xL7uBW8kxALYTKVsedcKRUG3WvL1bJiS179Huzt4epjjs8YgP6fbae-iT9zaszTP5sn_iuFIdEgM8e49cDjLBCwm6WmYHJKWdWk7CZzTWa3_hNdjihifdR1lpbYVQNop_pjzJcRHcJmak0-fU8gy8olBoyKX4QLJfsZNELmKrAx3E_WBfVL4If1gSc2i6RiSkCyUx2o-SxhZ4IQCqfcewzJzzO5Yp83W5Aq2LB_d255bFp3YDt7O4o0fbBocFciqFf3EMMhXAlJTttxxU0dZrDA8ilPmZic0JJjzkE8QG-3JneQ01lKLFwd49s9pFSiEJc29W0wq040jXx3hGCOmDbRmA2j9dpnw1ZupfgRBaE1PlXWd_BWYMPV7-dPIDDg1r8xK0mbn3dA3KO8HiUpsNsNGYpGMzE1Xk2iQVYfbvI7ZfVMHb-Zo-FcxiZweBLR-aUzgTrjlVoAJvoUIxQ-y8YjrK3D6quqEPLEkWGhEiCelH05Y_gfs7B11YQIA_R6Xs59ujWCKEGm2R28sg3NvE2H5oBYp4sMZvr06nVgzeq8M2q72ouMcAmf6SpGcSWxCJejIxsjEbN3xrCxOctTfXLnerZsJ9HLTahZZkGCDOfIB0pLFCMtwI6tgO82kkp4yWX7z11kXb4uKegftBZ2WLAUgf8VNe2QSxIdU2fi2ytxXeGQMapJp8vs6RcCdr7IvtwpmA8KrZ8iKR1jjKNjaQTxcscic1Q7nHH1JZ__cEFpH7OR82Rbe5Z_a__f1yTv0fWHAI9fK6Xm6QEI4FwuIL_xWtI-UivdAbQJmBA4JfcwxXHD4F2wlWEcMyGpUVz4xNiu1=w477-h357-no?authuser=0',
   },
   {
     name: 'Item 6',
     height: 600,
     width: 400,
+    src: 'https://lh3.googleusercontent.com/pw/ABLVV854juUtNiVdCR91ZX_YU2U51nqhSHke6YoLMpDDCCDXgP5y77HpgckJC2sWHpmbklj6IE6c7knkJ9oqSxlqa4y-wrYbBA_mRuxMUOesM_VZgwO_He3YCJoFcDBc4ppLCtCnktWUUKLLJt83xL7uBW8kxALYTKVsedcKRUG3WvL1bJiS179Huzt4epjjs8YgP6fbae-iT9zaszTP5sn_iuFIdEgM8e49cDjLBCwm6WmYHJKWdWk7CZzTWa3_hNdjihifdR1lpbYVQNop_pjzJcRHcJmak0-fU8gy8olBoyKX4QLJfsZNELmKrAx3E_WBfVL4If1gSc2i6RiSkCyUx2o-SxhZ4IQCqfcewzJzzO5Yp83W5Aq2LB_d255bFp3YDt7O4o0fbBocFciqFf3EMMhXAlJTttxxU0dZrDA8ilPmZic0JJjzkE8QG-3JneQ01lKLFwd49s9pFSiEJc29W0wq040jXx3hGCOmDbRmA2j9dpnw1ZupfgRBaE1PlXWd_BWYMPV7-dPIDDg1r8xK0mbn3dA3KO8HiUpsNsNGYpGMzE1Xk2iQVYfbvI7ZfVMHb-Zo-FcxiZweBLR-aUzgTrjlVoAJvoUIxQ-y8YjrK3D6quqEPLEkWGhEiCelH05Y_gfs7B11YQIA_R6Xs59ujWCKEGm2R28sg3NvE2H5oBYp4sMZvr06nVgzeq8M2q72ouMcAmf6SpGcSWxCJejIxsjEbN3xrCxOctTfXLnerZsJ9HLTahZZkGCDOfIB0pLFCMtwI6tgO82kkp4yWX7z11kXb4uKegftBZ2WLAUgf8VNe2QSxIdU2fi2ytxXeGQMapJp8vs6RcCdr7IvtwpmA8KrZ8iKR1jjKNjaQTxcscic1Q7nHH1JZ__cEFpH7OR82Rbe5Z_a__f1yTv0fWHAI9fK6Xm6QEI4FwuIL_xWtI-UivdAbQJmBA4JfcwxXHD4F2wlWEcMyGpUVz4xNiu1=w477-h357-no?authuser=0',
   },
 ];
+
 const LastYear = () => {
   const theme = useTheme();
   return (
@@ -78,14 +86,11 @@ const LastYear = () => {
                 alignItems="center"
                 justifyContent="center"
               >
-                <Box
-                  component={Avatar}
-                  width={200}
-                  height={200}
+                <Box 
+                  width={500} 
                   bgcolor={theme.palette.primary.main}
-                  color={theme.palette.background.paper}
                 >
-                  Campout Logo
+                  <BAAAHSLogo />
                 </Box>
               </Box>
             </Box>
@@ -101,20 +106,24 @@ const LastYear = () => {
             <Grid
               contentItems
               key={key}
-              xs={3}
+              xs={12}
+              md={6}
+              xl={4}
             >
               <Box display="flex" alignItems="center" justifyContent="center" marginBottom={4}>
-                <Box
+                <Box 
+                  component="img" 
+                  loading="lazy"
+                  src={item.src}
+                  alt={item.name}
                   width={item.width}
                   height={item.height}
-                  bgcolor={theme.palette.primary.main}
-                  color={theme.palette.background.paper}
-                  display="flex"
-                  alignItems="center" 
-                  justifyContent="center"
-                >
-                  {item.name}
-                </Box>
+                  sx={{
+                    objectFit: 'cover',
+                    filter:
+                    theme.palette.mode === 'dark' ? 'brightness(0.6)' : 'none',
+                  }}
+                />
               </Box>
             </Grid>
           ))}
