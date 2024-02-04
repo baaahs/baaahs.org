5717d83a5ce89d8f175e973d24f147fdc2d7c31c
Tom Seago <tom@tomseago.com>
2024-02-04 10:02:41 -0800

    Adding some sleeps since github seems to be rate limiting pulls

--- devops/merge_from_deploys.sh
+++ devops/merge_from_deploys.sh
@@ -2,6 +2,11 @@
 # Move to the parent directory
 cd "$(dirname "$0")/.." || exit
 
+green() {
+  msg=$1
+  echo -e "\033[32m${msg}\033[0m"
+}
+
 # Ensure script is on the main branch
 branch=$(git symbolic-ref --short -q HEAD)
 if [ "$branch" != "main" ]; then
@@ -16,26 +21,36 @@ if ! git diff --quiet; then
 fi
 
 # Pull from origin to make sure branches are up-to-date
+green "Pulling main to make sure we're up-to-date here..."
 if ! git pull origin main; then
   echo -e "\033[31mFailed to pull from origin. Please fix your conflicts.\033[0m"
   exit 1
 fi
+
+green "Sleeping for 2s to respect rate limits..."
+sleep 2
 
 # Declare an array for all deploy branches
 declare -a deploy_branches=("deploy-prod" "deploy-staging" "deploy-dev")
 
 # Iterate over each deploy branch and merge it into the 'main' branch
 for branch_name in "${deploy_branches[@]}"; do
+ green "Checkout and pull on $branch_name"
  git checkout "$branch_name"
  if ! git pull origin "$branch_name"; then
    echo -e "\033[31mFailed to pull from origin '$branch_name'. Please fix your conflicts.\033[0m"
    exit 1
  fi
+
+ green "Merging that down to main"
  git checkout main
  if ! git merge "$branch_name"; then
    echo -e "\033[31mFailed to merge '$branch_name' into 'main'. Please fix your conflicts.\033[0m"
    exit 1
  fi
+
+ green "Sleeping for 2s to respect rate limits..."
+ sleep 2
 done
 
-echo -e "\033[32mOperation completed successfully.\033[0m"
+green "Operation completed successfully."
