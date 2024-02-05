a68bdcca58fd2619b3127ddee28df5c6975a2173
Christian Williams <antixian666@gmail.com>
2024-02-05 15:48:25 -0800

    Remove /src/demos and /src/docs and routes.

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Routes as ReactRoutes, Route, Navigate } from 'react-router-dom';
 import viewsRoutes from 'views/routes';
-import docsRoutes from 'docs/routes';
 import blocksRoutes from 'blocks/routes';
-import demosRoutes from 'demos/routes';
 
 const Routes = () => {
   return (
@@ -11,13 +9,7 @@ const Routes = () => {
       {viewsRoutes.map((item, i) => (
         <Route key={i} path={item.path} element={item.renderer()} />
       ))}
-      {docsRoutes.map((item, i) => (
-        <Route key={i} path={item.path} element={item.renderer()} />
-      ))}
       {blocksRoutes.map((item, i) => (
-        <Route key={i} path={item.path} element={item.renderer()} />
-      ))}
-      {demosRoutes.map((item, i) => (
         <Route key={i} path={item.path} element={item.renderer()} />
       ))}
       <Route path="*" element={<Navigate replace to="/not-found-cover" />} />
