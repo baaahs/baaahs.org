86d7b9e3f57898404359000bcf3455b9b4253f4c
alejot127 <alejo.torro@icloud.com>
2024-02-14 12:25:05 -0800

    add link to event card

--- src/blocks/blog/HorizontallyAlignedBlogCardWithShapedImage/HorizontallyAlignedBlogCardWithShapedImage.js
+++ src/blocks/blog/HorizontallyAlignedBlogCardWithShapedImage/HorizontallyAlignedBlogCardWithShapedImage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useTheme } from '@mui/material/styles';
 import Divider from '@mui/material/Divider';
 import Card from '@mui/material/Card';
+import Link from '@mui/material/Link';
 import CardContent from '@mui/material/CardContent';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
@@ -18,6 +19,7 @@ const mock = {
     name: 'Clifford Hall',
     avatar: 'https://assets.maccarianagency.com/avatars/img1.jpg',
   },
+  link: '/campout',
 };
 
 const HorizontallyAlignedBlogCardWithShapedImage = ({ data }) => {
@@ -32,7 +34,9 @@ const HorizontallyAlignedBlogCardWithShapedImage = ({ data }) => {
     maxHeight,
     maxWidth,
     fontSize,
+    link,
   } = data || mock;
+
   return (
     <Container>
       <Box
@@ -49,101 +53,103 @@ const HorizontallyAlignedBlogCardWithShapedImage = ({ data }) => {
           },
         }}
       >
-        <Box
-          component={Card}
-          width={1}
-          height={1}
-          boxShadow={4}
-          display={'flex'}
-          flexDirection={{
-            xs: flip ? 'column' : 'column-reverse',
-            md: flip ? 'row-reverse' : 'row',
-          }}
-          sx={{ backgroundImage: 'none' }}
-        >
+        <Link underline="none" component="a" href={link}>
           <Box
-            sx={{
-              width: { xs: 1, md: '50%' },
-              position: 'relative',
-              display: 'flex',
+            component={Card}
+            width={1}
+            height={1}
+            boxShadow={4}
+            display={'flex'}
+            flexDirection={{
+              xs: flip ? 'column' : 'column-reverse',
+              md: flip ? 'row-reverse' : 'row',
             }}
+            sx={{ backgroundImage: 'none' }}
           >
             <Box
-              component={'img'}
-              height={1}
-              width={1}
-              src={image}
-              alt="..."
               sx={{
-                width: maxWidth ?? '100%',
-                height: maxHeight ?? '100%',
-                objectFit: 'cover',
-                filter:
-                  theme.palette.mode === 'dark' ? 'brightness(0.7)' : 'none',
+                width: { xs: 1, md: '50%' },
+                position: 'relative',
+                display: 'flex',
               }}
-            />
-            <Box
-              component={'svg'}
-              viewBox="0 0 112 690"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              sx={{
-                position: 'absolute',
-                bottom: 0,
-                top: '-50%',
-                left: 0,
-                right: 0,
-                color: theme.palette.background.paper,
-                transform: 'scale(2)',
-                height: 1,
-                width: 'auto',
-                transformOrigin: 'top center',
-                display: { xs: 'none', md: 'block' },
-              }}
-            />
-          </Box>
-          <CardContent
-            sx={{
-              position: 'relative',
-              width: { xs: 1, md: '50%' },
-              padding: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'space-between',
-            }}
-          >
-            <Box>
-              <Typography
-                variant={'h5'}
-                gutterBottom
-                fontFamily={'Smooth Circulars'}
-              >
-                {title}
-              </Typography>
-              <Typography color="text.secondary" fontSize={fontSize}>
-                {description}
-              </Typography>
-            </Box>
-            <Box>
-              <Divider sx={{ marginY: 2 }} />
+            >
               <Box
-                display={'flex'}
-                justifyContent={'space-between'}
-                alignItems={'center'}
-              >
-                {author && (
-                  <Box display={'flex'} alignItems={'center'}>
-                    <Avatar src={author.avatar} sx={{ marginRight: 1 }} />
-                    <Typography color={'text.secondary'}>
-                      {author.name}
-                    </Typography>
-                  </Box>
-                )}
-                <Typography color={'text.secondary'}>{date}</Typography>
-              </Box>
+                component={'img'}
+                height={1}
+                width={1}
+                src={image}
+                alt="..."
+                sx={{
+                  width: maxWidth ?? '100%',
+                  height: maxHeight ?? '100%',
+                  objectFit: 'cover',
+                  filter:
+                    theme.palette.mode === 'dark' ? 'brightness(0.7)' : 'none',
+                }}
+              />
+              <Box
+                component={'svg'}
+                viewBox="0 0 112 690"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                sx={{
+                  position: 'absolute',
+                  bottom: 0,
+                  top: '-50%',
+                  left: 0,
+                  right: 0,
+                  color: theme.palette.background.paper,
+                  transform: 'scale(2)',
+                  height: 1,
+                  width: 'auto',
+                  transformOrigin: 'top center',
+                  display: { xs: 'none', md: 'block' },
+                }}
+              />
             </Box>
-          </CardContent>
-        </Box>
+            <CardContent
+              sx={{
+                position: 'relative',
+                width: { xs: 1, md: '50%' },
+                padding: 4,
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+              }}
+            >
+              <Box>
+                <Typography
+                  variant={'h5'}
+                  gutterBottom
+                  fontFamily={'Smooth Circulars'}
+                >
+                  {title}
+                </Typography>
+                <Typography color="text.secondary" fontSize={fontSize}>
+                  {description}
+                </Typography>
+              </Box>
+              <Box>
+                <Divider sx={{ marginY: 2 }} />
+                <Box
+                  display={'flex'}
+                  justifyContent={'space-between'}
+                  alignItems={'center'}
+                >
+                  {author && (
+                    <Box display={'flex'} alignItems={'center'}>
+                      <Avatar src={author.avatar} sx={{ marginRight: 1 }} />
+                      <Typography color={'text.secondary'}>
+                        {author.name}
+                      </Typography>
+                    </Box>
+                  )}
+                  <Typography color={'text.secondary'}>{date}</Typography>
+                </Box>
+              </Box>
+            </CardContent>
+          </Box>
+        </Link>
       </Box>
     </Container>
   );
