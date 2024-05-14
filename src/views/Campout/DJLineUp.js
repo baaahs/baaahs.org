553fe0f6067cf8958ac0915e9a0d5d2eb84afb1b
Christian Williams <antixian666@gmail.com>
2024-05-13 20:55:36 -0700

    Add DJ bios and more images.

--- src/views/Campout/DJLineUp.js
+++ src/views/Campout/DJLineUp.js
@@ -25,220 +25,230 @@ const DJLineup = () => {
         <Grid container marginTop={2} spacing={2} justifyContent='center'>
           <Grid container item xs={12} xl={8} spacing={2}>
             <DJ
-              index={0}
               name="Kelly Naughton"
-              blurb="Disco, House, Techno"
+              affiliation="DAD SF, BAAAHS"
+              bio="Kelly Naughton is a San Francisco based DJ focusing on
+               delectable Disco, racy Italo, sassy House, & thumping Techno.
+               He created DAD SF, a disco/vers unabashedly joyous dance party
+               in SF's SOMA district."
               soundcloud="https://soundcloud.com/kellynaughton"
               instagram="https://www.instagram.com/naughtontime"
               image="https://storage.googleapis.com/static.baaahs.org/z_kelly.png"
-              key={0}
             />
             <DJ
-              index={1}
               name="Booty Juice"
-              blurb="Disco, House, Techno, Hip-Hop"
+              affiliation="DAD SF, BAAAHS"
+              bio="San Francisco's Wettest DJ. 🪩 Noise Polluter.
+               Hot Mess & Neighborhood Friendly Dumpster Fire."
               soundcloud="https://soundcloud.com/bootyjuicesf"
               instagram="https://www.instagram.com/bootyjuicesf"
               image="https://storage.googleapis.com/static.baaahs.org/z_booty.png"
-              key={1}
             />
             <DJ
-              index={2}
               name="StormyRoxx"
-              blurb="Disco, House, Techno"
+              affiliation="Lust (PDX), The Locker Room (PDX), Shift Festival"
+              bio="A cool daddy who plays cool music."
               soundcloud="https://soundcloud.com/stormyroxx"
               instagram="https://www.instagram.com/stormyroxx"
               image="https://storage.googleapis.com/static.baaahs.org/z_stormy.png"
-              key={2}
             />
             <DJ
-              index={3}
               name="Tiny Tim"
-              blurb="Funky/Jackin’ House, Funk/Soul, Disco, Soulful House, R&B, Deep House, Tech House and Nu Disco."
+              affiliation="Something Queer, Chonies"
+              bio="Combining 21 years of mixing with production, I love to
+               produce and play music that comes from the soul, goes to the
+               decks and is felt on the floor!"
               soundcloud="https://www.soundcloud.com/djtinytp"
               instagram="https://www.instagram.com/djtinytp"
               image="https://storage.googleapis.com/static.baaahs.org/z_tiny.png"
-              key={3}
             />
             <DJ
-              index={4}
               name="Trevor Sigler"
-              blurb="House, Techno"
+              affiliation="Ever Afters, EW"
+              bio=""
               soundcloud="https://on.soundcloud.com/jiaZYGot2TF5eEfo8"
               instagram="https://www.instagram.com/tater_swift"
               image="https://storage.googleapis.com/static.baaahs.org/z_trevor.png"
-              key={4}
             />
             <DJ
-              index={5}
               name="Del"
-              blurb="Baile Funk, Jackin House, Jersey Club, Hard Techno, Dubstep"
+              affiliation="Port Bar, Steamworks (Berkeley), Eli's Mile High Club"
+              bio="As a pioneer in bringing Jersey Club to the Bay Area,
+               her high energy cross genre sets and flawless blends are
+               guaranteed to make any dance floor shake."
               soundcloud="https://on.soundcloud.com/HKpGb"
               instagram="https://www.instagram.com/inebriatedfelon"
               image="https://storage.googleapis.com/static.baaahs.org/z_del.png"
-              key={5}
             />
             <DJ
-              index={6}
               name="Romii"
-              blurb="House, Techno, Breakbeat, Trance, Darkwave, Lofi/Downtempo/Memphis, Rap/Alt-Rock"
+              affiliation="DreamTension"
+              bio="In addition to his background as a competitive classical
+               solo-violinist, Romii is influenced by Motown era artists,
+               80’s soul, 90’s hiphop and Detroit synthetic underground sounds."
               soundcloud="https://soundcloud.com/kingromii"
               instagram="https://www.instagram.com/romii_dreamtension"
               image="https://storage.googleapis.com/static.baaahs.org/z_romii.png"
-              key={6}
             />
             <DJ
-              index={7}
-              name="GUERRILLA PUMP"
-              blurb="GHETTO HOUSE, ELECTRO BOOTY BASS AND BALLROOM (VOGUE)"
-              soundcloud="https://youtu.be/MkYdduxP8Pc?si=M5p2lL9VyziP5JeO&t=693"
+              name="Guerrilla Pump"
+              affiliation="Pump Paradise"
+              bio="DJ Guardian Guerrilla Pump aka Sailor G is a Queer 1st Gen
+               Afro-Pinoy, Award Winning DJ and Community-based Artist/Organizer
+               rooted in the East Bay’s Underground Scene."
+              soundcloud="https://soundcloud.com/guerrillapump"
               instagram="https://www.instagram.com/shotbyguerrilla"
-              image=""
-              key={7}
+              image="/images/campout/2024/djs/guerrillapump.jpg"
             />
             <DJ
-              index={8}
               name="Issa"
-              blurb="House, Disco, Hip-Hop, Techno, World Music"
+              affiliation=""
+              bio="Does anyone want my miso soup?"
               soundcloud="https://soundcloud.com/itsissa"
               instagram="http://instagram.com/issaqraj"
               image="https://storage.googleapis.com/static.baaahs.org/z_issa.png"
-              key={8}
             />
             <DJ
-              index={9}
               name="Twunkerbell"
-              blurb="Tech House, EDM, Mashups, Disco, Trance"
+              affiliation="BAAAHS"
+              bio="Music connoisseurs and
+               headbanging ravers alike have referred to his DJ sets as
+               &quot;decent&quot;, &quot;pretty good&quot;, and &quot;something
+               people listen to&quot;. Combining 20 years of classical piano
+               training with complementary musical genres such as
+               slut pop, tech house, and nu disco, he incorporates live
+               elements into his high-energy sets to take your ears on a magical
+               journey of sunshine, rainbows, and unicorn ketamine."
               soundcloud="https://soundcloud.com/twunkerbell"
               instagram=""
               image="https://storage.googleapis.com/static.baaahs.org/z_twunkerbell.png"
-              key={9}
             />
             <DJ
-              index={10}
               name="Mondo Nexus"
-              blurb="Sleazy Synthy Disco mixed with Groovin House with a touch of Acid"
+              affiliation="DAD SF"
+              bio="Sleazy Synthy Disco mixed with Groovin House with a touch of Acid"
               soundcloud="https://soundcloud.com/mondonexus"
               instagram="https://www.instagram.com/mondonexus"
               image="https://storage.googleapis.com/static.baaahs.org/z_mondo.png"
-              key={10}
             />
             <DJ
-              index={11}
               name="Prince Wolf"
-              blurb="Disco, House, Techno"
+              affiliation="THRUST"
+              bio="If the energy of moonlit rendezvous could be distilled into
+               sound, Prince Wolf's sets embody that: thrilling, clandestine
+               encounters with just a tinge of danger. Music that keeps you
+               alert to the prying eyes & enticing bodies gyrating on the
+               dancefloor."
               soundcloud=""
               instagram="https://www.instagram.com/somaqlo"
               image="https://storage.googleapis.com/static.baaahs.org/z_prince_wolf.png"
-              key={11}
             />
             <DJ
-              index={12}
               name="Sir Spense"
-              blurb="Disco Pop Good vibes :)"
+              affiliation="DAD SF, BAAAHS"
+              bio="Disco Pop Good vibes :)"
               soundcloud=""
               instagram=""
               image="https://storage.googleapis.com/static.baaahs.org/z_spenser.png"
-              key={12}
             />
             <DJ
-              index={13}
               name="Bryan Hughes"
-              blurb="Progressive, Trance/Morning, Disco, House, Gospel House"
+              bio="Progressive, Trance/Morning, Disco, House, Gospel House"
               soundcloud="https://soundcloud.com/djbryanhughes"
               instagram=""
               image="https://storage.googleapis.com/static.baaahs.org/z_bryan_hughes.png"
-              key={13}
             />
             <DJ
-              index={14}
               name="Brian Maier"
-              blurb="Progressive House, Melodic House, Techno"
+              bio="San Francisco-based DJ Brian Maier blends his love for house
+               music and technology, performing at global venues and events like
+              Toronto Pride and the legendary Real Bad party."
               soundcloud="https://soundcloud.com/brianmaier"
               instagram=""
-              image=""
-              key={14}
+              image="/images/campout/2024/djs/brianmaier-crop.jpg"
             />
             <DJ
-              index={15}
               name="John Major"
-              blurb="House/Minimal Techno/Punk/Polyrhythmic"
+              affiliation="BTW"
+              bio="House/Minimal Techno/Punk/Polyrhythmic"
               soundcloud="https://soundcloud.com/majorjohn"
               instagram=""
-              image=""
-              key={15}
+              image="/images/campout/2024/djs/major.jpg"
             />
             <DJ
-              index={16}
               name="Aphex"
-              blurb="Trance, Melodic Techno, Progressive House"
+              affiliation="BAAAHS"
+              bio="Trance, Melodic Techno, Progressive House"
               soundcloud="https://soundcloud.com/aphexcx"
               instagram="http://aphex.dj"
               image="https://storage.googleapis.com/static.baaahs.org/z_aphek.png"
-              key={16}
             />
             <DJ
-              index={17}
               name="Mike Bradley"
-              blurb="House, Techno, Tech House. Always fun with a splash of sass~"
+              affiliation=""
+              bio="House, Techno, Tech House. Always fun with a splash of sass~"
               soundcloud="https://m.mixcloud.com/mikebradley9216/"
               instagram=""
               image="https://storage.googleapis.com/static.baaahs.org/z_mike_b.png"
-              key={17}
             />
             <DJ
-              index={18}
               name="Dennis Dashing"
-              blurb="Techno, House"
+              bio="Techno, House"
               soundcloud="https://on.soundcloud.com/u7ATk"
               instagram="https://www.instagram.com/dennisdashing"
               image="https://storage.googleapis.com/static.baaahs.org/z_dennis_dashing.png"
-              key={18}
             />
             <DJ
-              index={19}
               name="Dunst"
-              blurb="Techno, Eelectro, Detroit, Bass, Breaks, Leftfield, IDM, Industrial, Synth-punk"
+              bio="Techno, Eelectro, Detroit, Bass, Breaks, Leftfield, IDM, Industrial, Synth-punk"
               soundcloud=""
               instagram=""
-              image=""
-              key={19}
+              image="/images/campout/2024/djs/dunst.jpg"
             />
             <DJ
-              index={20}
               name="hunnycut"
-              blurb="Queer Centered G-Tech & Bass House"
+              bio="Queer Centered G-Tech & Bass House"
               soundcloud="https://soundcloud.com/hunnycut"
               instagram="https://www.instagram.com/jckc.ny"
               image=""
-              key={20}
             />
             <DJ
-              index={21}
               name="LOVR"
-              blurb="Dirty bass, music to fuck to, a walk through the hood that makes you feel unsafe."
+              bio="Dirty bass, music to fuck to, a walk through the hood that makes you feel unsafe."
               soundcloud=""
               instagram=""
               image=""
-              key={21}
             />
             <DJ
-              index={22}
               name="LAKEVERETT"
-              blurb="House, Hip-Hop, Baile Funk, Breakbeat, Disco, Electro, Bass and R&B"
+              affiliation="Callback"
+              bio="House, Hip-Hop, Baile Funk, Breakbeat, Disco, Electro, Bass and R&B"
               soundcloud="https://soundcloud.com/lakeverett"
               instagram="https://www.instagram.com/lakeverett"
               image=""
-              key={22}
             />
             <DJ
-              index={23}
               name="State of Wayne"
-              blurb=""
+              bio=""
               soundcloud=""
               instagram="https://www.instagram.com/waynebone"
               image=""
-              key={23}
+            />
+            <DJ
+              name="Mr Seago"
+              affiliation="BAAAHS, Something Queer"
+              bio="Funky crazy bitch."
+              soundcloud="https://soundcloud.com/mrseago"
+              instagram="https://www.instagram.com/mrseago"
+              image="/images/campout/2024/djs/mrseago.jpg"
+            />
+            <DJ
+              name="eMote"
+              affiliation="BAAAHS"
+              bio="Funky crazy bitch."
+              soundcloud="https://soundcloud.com/mrseago"
+              instagram="https://www.instagram.com/mrseago"
+              image="/images/campout/2024/djs/emote.jpg"
             />
           </Grid>
         </Grid>
@@ -247,11 +257,11 @@ const DJLineup = () => {
   );
 };
 
-const DJ = ({ index, name, image, blurb, soundcloud, instagram }) => {
+const DJ = ({ name, image, affiliation, bio, soundcloud, instagram }) => {
   const theme = useTheme();
 
   return (
-    <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={index}>
+    <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
       <Box
         alignItems="center"
         component={Card}
@@ -259,8 +269,6 @@ const DJ = ({ index, name, image, blurb, soundcloud, instagram }) => {
         borderRadius={2}
         width={1}
         height={1}
-        data-aos={'fade-up'}
-        data-aos-delay={index * 100}
       >
         <Box flexDirection={'row'} alignItems="center" gap={2}>
           <Box
@@ -294,8 +302,18 @@ const DJ = ({ index, name, image, blurb, soundcloud, instagram }) => {
             >
               {name}
             </Typography>
+
+            {affiliation && (
+              <Box>{
+                affiliation.split(', ').map((aff) => (
+                  <Typography color="text.secondary"
+                    textAlign="center">{aff}</Typography>
+                ))
+              }</Box>
+            )}
+
             <Typography color="text.secondary" textAlign="center">
-              {blurb}
+              {bio}
             </Typography>
             <Box
               display={'flex'}
